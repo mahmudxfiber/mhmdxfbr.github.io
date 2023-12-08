e1a88b944b07c2f5781d7c335ec57e2bfbd5182b
mahmudxfiber <153246291+mahmudxfiber@users.noreply.github.com>
2023-12-08 09:58:12 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,73 @@
+<?php include 'header.php';?>
+      
+      <div class="jumbotron jumbotron-fluid">
+        <div class="container text-center">
+            <img class="rounded-circle" width="300" height="300" src="https://radarbanyumas.disway.id/upload/00a24f9ba31136c5787fb1a912d35acd.jpg" alt="">
+          <h1 class="display-4">Kabupaten Banyumas</h1>
+          <p class="lead">KOTA SATRIA</p>
+        </div>
+      </div>
+
+  <div class="container text-center" id="About">
+    <div class="row">
+    <div class="col">
+            <h1>Tentang Banyumas</h1>
+  </div>
+  </div>
+    <div class="row">
+    <div class="col text-justify">
+        <p>Kabupaten Banyumas (Jawa: ꦧꦚꦸꦩꦱ꧀) adalah sebuah wilayah kabupaten yang terletak di Provinsi Jawa Tengah, Indonesia. Ibu kotanya adalah Kota Purwokerto, kabupaten ini berbatasan dengan Kabupaten Brebes di utara; Kabupaten Purbalingga, Kabupaten Banjarnegara, dan Kabupaten Kebumen di timur, serta Kabupaten Cilacap di sebelah selatan dan barat. Gunung Slamet, gunung tertinggi di Jawa Tengah terdapat di ujung utara wilayah kabupaten ini.
+
+          Kabupaten Banyumas merupakan bagian dari wilayah budaya Banyumasan, yang berkembang di bagian barat Jawa Tengah. Bahasa yang dituturkan adalah bahasa Banyumasan, atau yang lebih akrab disebut Ngapak, yaitu salah satu ragam dialek bahasa Jawa.</p>
+</div>
+<div class="col text-justify">
+        <p>Secara astronomis, Kabupaten Banyumas terletak antara 7°15'05"–7°37'10" Lintang Selatan dan antara 108°39'17"–109°27'15" Bujur Timur. Luas wilayah Kabupaten Banyumas sekitar 1.327,60 km2 atau setara dengan 132.759,56 ha, dengan keadaan wilayah antara daratan dan pegunungan dengan struktur pegunungan terdiri dari sebagian lembah Sungai Serayu untuk tanah pertanian, sebagian dataran tinggi untuk pemukiman dan pekarangan, dan sebagian pegunungan untuk perkebunan dan hutan tropis terletak di lereng Gunung Slamet sebelah selatan.</p>
+</div>
+</div>
+</div>
+<div class="container text-center">
+<div class="row">
+<div class="col">
+    <h1>Portofolio</h1>
+</div>
+</div>
+
+  <div class="card-deck mt-5 ml-5 mr-5">
+    <div class="card">
+      <img class="card-img-top" src="https://www.goodnewsfromindonesia.id/uploads/images/2022/11/0122182022-menara-pandang.png" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Menara Teratai</h5>
+        <p class="card-text">Menara Teratai Purwokerto</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 week ag</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://radarbanyumas.disway.id/upload/2021/12/FOTO-A-2.jpg" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Baturraden</h5>
+        <p class="card-text">Wisata Baturraden</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 week ago</small></p>
+      </div>
+    </div>
+    <div class="card">
+      <img class="card-img-top" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQoUhHliBm5miZvCh0jKBlawmHwJ9n04s4n2A&usqp=CAU" alt="Card image cap">
+      <div class="card-body">
+        <h5 class="card-title">Kecamatan Banyumas</h5>
+        <p class="card-text">Alun-alun Banyumas</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 week ag</small></p>
+      </div>
+    </div>
+  </div>
+
+
+ 
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.3/dist/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
+
+<?php include 'footer.php';?>
